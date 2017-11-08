330f630cfd9a9f674c6be138f1dbd8fb1930c8c0
Will Carty <cartycrabber@gmail.com>
2017-11-08 02:57:06 -0500

    Populate rat list, reorganize project files

---
+++ Rat App/Model/SIghting.cs
@@ -0,0 +1,68 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using Rat_App;
+//
+//    var data = Sighting.FromJson(jsonString);
+//
+namespace Rat_App
+{
+    using System;
+    using System.Net;
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public partial class Sighting
+    {
+        [JsonProperty("borough")]
+        public string Borough { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("dateCreated")]
+        public DateTime DateCreated { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("locationType")]
+        public string LocationType { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("streetAddress")]
+        public string StreetAddress { get; set; }
+
+        [JsonProperty("zipcode")]
+        public long Zipcode { get; set; }
+
+        public override string ToString()
+        {
+            return $"Sighting ID: {Id}, Date Created: {DateCreated.ToString("MM-dd-yyyy")}";
+        }
+    }
+
+    public partial class Sighting
+    {
+        public static Sighting[] FromJson(string json) => JsonConvert.DeserializeObject<Sighting[]>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Sighting[] self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    public class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+        };
+    }
+}
--- Rat App/View/MainPage.xaml.cs
+++ Rat App/View/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +27,25 @@ namespace Rat_App
         public MainPage()
         {
             this.InitializeComponent();
+            PopulateListView();
+        }
+
+        private async void PopulateListView()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://muffindreamers.azurewebsites.net/sightings");
+            request.ContentType = "application/json";
+
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            Sighting[] sightings;
+            using(StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                sightings = Sighting.FromJson(reader.ReadToEnd());
+            }
+
+            foreach(Sighting s in sightings)
+            {
+                listViewSightings.Items.Add(s);
+            }
         }
 
         private async void buttonLogout_Click(object sender, RoutedEventArgs e)
