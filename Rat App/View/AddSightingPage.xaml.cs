1ebf637580e17fcb9b5157804e5c8bca4b9b13db
Will Carty <cartycrabber@gmail.com>
2017-11-08 20:53:17 -0500

    Check zipcode, latitude, and longitude textbox input to ensure they are numerical

--- Rat App/View/AddSightingPage.xaml.cs
+++ Rat App/View/AddSightingPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,8 +23,6 @@ namespace Rat_App.View
     /// </summary>
     public sealed partial class AddSightingPage : Page
     {
-        //TODO: Zipcode, latitude, longitude should only accept numerical input
-
         public AddSightingPage()
         {
             this.InitializeComponent();
@@ -37,6 +36,29 @@ namespace Rat_App.View
         private void buttonCancel_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(MainPage));
+        }
+
+        private void IntTextbox(object sender, KeyRoutedEventArgs args)
+        {
+            VirtualKey[] allowedKeys = {VirtualKey.Number0, VirtualKey.Number1, VirtualKey.Number2,
+                VirtualKey.Number3, VirtualKey.Number4, VirtualKey.Number5, VirtualKey.Number6,
+                VirtualKey.Number7, VirtualKey.Number8, VirtualKey.Number9, VirtualKey.Decimal};
+
+            if(!allowedKeys.Contains(args.Key))
+            {
+                args.Handled = true;
+            }
+        }
+
+        private void DoubleTextBox(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            double dtemp;
+            if (!double.TryParse(sender.Text, out dtemp) && sender.Text != "")
+            {
+                int pos = sender.SelectionStart - 1;
+                sender.Text = sender.Text.Remove(pos, 1);
+                sender.SelectionStart = pos;
+            }
         }
     }
 }
